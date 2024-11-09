9321eb24899b522e23cf33c13ee0ce54e91c7c86
Faraaz Ansari <faraaz.faiz.ansari@gmail.com>
2024-11-09 11:14:19 +0530

    Add files via upload
    Initial commit

---
+++ script.js
@@ -0,0 +1,38 @@
+let idCount = 0;
+document.querySelector("#add-btn").addEventListener("click", addCheckList);
+
+function addCheckList() {
+  idCount += 1;
+  document.querySelector(".text-msg").classList.add("hide");
+  document.querySelector(".task-container").insertAdjacentHTML(
+    "beforeend",
+    `<div class="task-info">
+            <input type="checkbox" name="checkbox" id="task${idCount}">
+            <label for="task${idCount}">
+                <div class="unchecked"></div>
+                <div class="checked"></div>
+            </label>
+            <div contenteditable="true"></div>
+            <button class="clear" onClick="clearBtnClicked(event)"></button>
+        </div>`
+  );
+  document.querySelector(`.task-info #task${idCount}~div`).focus();
+  updateTaskCount();
+}
+
+function clearBtnClicked(event) {
+  document
+    .querySelector(".task-container")
+    .removeChild(event.target.parentElement);
+  idCount -= 1;
+  updateTaskCount();
+}
+
+function updateTaskCount() {
+  document.querySelector(".user-heading span").innerText =
+    idCount + (idCount === 1 ? " Entry" : " Entries");
+
+  if (idCount === 0) {
+    document.querySelector(".text-msg").classList.remove("hide");
+  }
+}
